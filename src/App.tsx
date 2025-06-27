76fdd30591b858f3ac067206e89c38345683d3d4
tompointcom <tom.courcy@gmail.com>
2025-06-27 16:55:54 +0200

    Implement Footer with styling for consistent layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { CreateEmployee } from './pages/CreateEmployee/CreateEmployee'
-import Header from './components/features/layout/Header'
+import Header from './components/features/layout/Header/Header'
+import Footer from './components/features/layout/Footer/Footer'
 
 function App() {
   return (
     <>
       <Header/>
       <CreateEmployee />
+      <Footer />
     </>
   )
 }
---
+++ src/components/features/layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer: React.FC = () => (
+    <footer className={styles.footer}>
+        <div className={styles.footerContent}>
+            <p>&copy; {new Date().getFullYear()} Wealth Health. All rights reserved.</p>
+        </div>
+    </footer>
+);
+
+export default Footer;
